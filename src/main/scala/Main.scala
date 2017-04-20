9d50232b57967c84de969a8575d3fd7194830f0c
Charmy Garg <charmy.garg@knoldus.in>
2017-04-20 16:07:09 +0530

    Spark Assignment 1 PageCount

---
+++ src/main/scala/Main.scala
@@ -0,0 +1,21 @@
+
+object Main extends App {
+
+  import GlobalObject._
+
+  val pageCount = new PageCount
+
+  println("Total of Page Count is: " + pageCount.getTotalPageCount)
+
+  println("Top ten by hit : ")
+
+  pageCount.getTopTen.foreach(println)
+
+  println("English Page Count: " + pageCount.getEnglishCount)
+
+  println("Count of Page Hit : " + pageCount.getPageHit)
+
+  println(pageCount.getARType)
+
+  sparkContext.stop()
+}
